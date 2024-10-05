51ca5760e21ea88739be108b91f9b0bc05ab9ea7
Patrick Wilmes <p.wilmes89@gmail.com>
2024-10-05 07:06:42 +0200

    Configuration for db
    Now the database configuration is not hardcoded anymore, but rather
    loaded from the forge.ini configuration file.

--- Libraries/LibStorage/PostgresqlFileIndexStore.cpp
+++ Libraries/LibStorage/PostgresqlFileIndexStore.cpp
@@ -8,13 +8,29 @@
 #include "IO.h"
 #include "IndexFile.h"
 #include "Logging.h"
+#include "UserSpace.h"
 #include <exception>
 #include <memory>
 #include <stdexcept>
 
 #define POSTGRES_INIT_SQL "Resources/DB/file_table.sql"
 
-LibStorage::PostgresqlFileIndexStore::PostgresqlFileIndexStore(): _connection("host=127.0.0.1 port=5433 dbname=forgebase user=root password=123") {
+static std::string build_connection_string() {
+    AK::UserSpace user_space;
+    auto host = user_space.get_property("db_host");
+    auto port = user_space.get_property("db_port");
+    auto user = user_space.get_property("db_user");
+    auto password = user_space.get_property("db_password");
+    auto name = user_space.get_property("db_name");
+    AK::debug("DB Host: " + host);
+    AK::debug("DB Port: " + port);
+    AK::debug("DB User: " + user);
+    AK::debug("DB Password: " + password);
+    AK::debug("DB Name: " + name);
+    return "host=" + host + " port=" + port + " dbname=" + name + " user=" + user + " password=" + password;
+}
+
+LibStorage::PostgresqlFileIndexStore::PostgresqlFileIndexStore(): _connection(build_connection_string()) {
     _user_space = std::make_unique<AK::UserSpace>();
     try {
         if (!_connection.is_open()) {
@@ -36,7 +52,11 @@ LibStorage::PostgresqlFileIndexStore::~PostgresqlFileIndexStore() {
 
 void LibStorage::PostgresqlFileIndexStore::write_to_index(const std::shared_ptr<std::vector<IndexFile> > &files) {
     if (_connection.is_open()) {
-        _connection.prepare("insert_file", "INSERT INTO files (path, type, name) values ($1, $2, $3);");
+        _connection.prepare("insert_file", 
+                "INSERT INTO files (path, type, name) values ($1, $2, $3)"
+                "ON CONFLICT (path) "
+                "DO UPDATE SET type=EXCLUDED.type, name=EXCLUDED.name;"
+            );
         pqxx::work txn(_connection);
         for (auto file : *files) {
             txn.exec_prepared("insert_file", file.get_path(), 0, file.get_name());
